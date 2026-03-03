7716e7b176fc171a4f2c12ce0e5c40ce0ea6b7f0
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-03 04:45:37 +0300

    Created the update, delete and create routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,25 @@ app.get("/api/notes", (req, res) => {
 res.status(200).send("you got 5 notes")
 })
 
+/* CREATE ITEM */
+
+app.post("/api/notes", (req, res) => {
+res.status(201).send("you got 5 notes")
+})
+
+/* UPDATE ITEM */
+
+app.put("/api/notes/:id", (req, res) => {
+res.status(200).send("Note created successfully!")
+})
+
+/* DELETE ITEM */
+
+app.put("/api/notes/:id", (req, res) => {
+res.status(200).send("Note deleted successfully!")
+})
+
+
 app.listen(3000, () => {
   console.log("Server started on PORT: 5001");
 })
