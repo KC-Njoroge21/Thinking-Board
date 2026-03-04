6602d315c4b1e347e154bb5256e8cb5d65a7b223
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-05 01:26:04 +0300

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,21 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 //middleware
-app.use(express.json())
+app.use(express.json())  // this middleware will parse JSON bodies
+
+app.use((req, res, next) => {
+  console.log("We just got a new Request");
+  next();
+})
 
 app.use("/api/notes", notesRoutes)
 
-connectDB();
-
-
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
   console.log("Server started on PORT:", PORT);
 }) 
+})
+
+
+
 
--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes  (req, res)  {
   try {
-    const notes = await Note.find()
+    const notes = await Note.find().sort({createAt: -1}) //will sort in order of newest first
     res.status(200).json(notes)
   } catch ( error) {
     res.status(500).json({message:"Internal server error"})
