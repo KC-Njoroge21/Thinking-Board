b4ce46d42a82d353ae99ddc9390487859c2a420a
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-05 00:52:43 +0300

    Updated my deleteNote controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -26,14 +26,31 @@ export async function createNote (req, res) {
 export async function updateNote (req, res) {
   try {
     const {title, content} = req.body
-    await Note.findByIdAndUpdate(req.params.id, {title, content})
-    res.status(200).json({message: "Note deleted successfully"})
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content}, {new: true})
+  if(!updatedNote) {
+    return (
+      res.status(404).json({message:"Note not found"})
+    )
+  }
+
+      res.status(200).json({message: updatedNote})
   } catch (error)  {
     console.error("Error in updateNote controller", error);
     res.status(500).json({message: "Internal server error"})
   }
 };
 
-export const deleteNote = (req, res) => {
-  res.status(200).send("Note deleted successfully");
+export async function deleteNote (req, res) {
+  try {
+    await Note.findByIdAndDelete(req.params.id) 
+    if(!deleteNote) {
+      return (
+        res.status(404).json({message: "Note not found"})
+      )
+    }
+    res.status(200).json({message: "Note deleted successfully!"})
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({message: "Internal server error"})
+  }
 };
